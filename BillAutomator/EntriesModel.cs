834b1f0df4646dd5bcca73158d587d3cff434f9f
Chris Purkiss <51557567+cranberrykreme@users.noreply.github.com>
2021-11-30 17:09:19 +1100

    Add project files.

---
+++ BillAutomator/EntriesModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillAutomator
+{
+    internal class EntriesModel
+    {
+        public DateTime date { get; set; }
+
+        public List<SolicitorsModel> solicitor { get; set; } = new List<SolicitorsModel>();
+
+        public string description { get; set; }
+
+        public double hours { get; set; }
+
+        public double amount { get; set; }
+    }
+}
---
+++ BillAutomator/SolicitorsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillAutomator
+{
+    internal class SolicitorsModel
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string initials { get; set; }
+
+        public List<double> hourlyRates { get; set; }
+
+        public DateTime dateOfAdmission { get; set; }
+
+        public List<DateTime> datesOfHourlyRates { get; set; }
+    }
+}
